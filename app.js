9e2c46f34ff71c9b2f146d76a7cbfcc42e0bbc6c
Tri Nguyen <tri282@gmail.com>
2019-11-21 20:03:22 -0800

    change api router location

--- app.js
+++ app.js
@@ -91,15 +91,7 @@ app.use(session({
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-if(coins[config.coin].api) {
-	var rateLimit = require("express-rate-limit");
-	var apiProperties = coins[config.coin].api();
-	var limiter = rateLimit(apiProperties.limit);
-	var apiRounter = express.Router();
-	app.use(apiProperties.base_uri, limiter);
-	var restfullAPI = new Restfull(apiRounter, coins[config.coin].api());
-	app.use(apiProperties.base_uri, apiRounter);
-}
+
 process.on("unhandledRejection", (reason, p) => {
 	debugLog("Unhandled Rejection at: Promise", p, "reason:", reason, "stack:", (reason != null ? reason.stack : "null"));
 });
@@ -418,6 +410,15 @@ app.use(csurf(), (req, res, next) => {
 });
 
 app.use('/', baseActionsRouter);
+if(coins[config.coin].api) {
+	var rateLimit = require("express-rate-limit");
+	var apiProperties = coins[config.coin].api();
+	var limiter = rateLimit(apiProperties.limit);
+	var apiRounter = express.Router();
+	app.use(apiProperties.base_uri, limiter);
+	var restfullAPI = new Restfull(apiRounter, coins[config.coin].api());
+	app.use(apiProperties.base_uri, apiRounter);
+}
 
 app.use(function(req, res, next) {
 	var time = Date.now() - req.startTime;
