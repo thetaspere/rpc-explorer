412041afd361fabc80bf1df1d455b88556abf95c
Tri Nguyen <tri282@gmail.com>
2019-11-21 19:57:11 -0800

    added limiter

--- app.js
+++ app.js
@@ -93,6 +93,7 @@ app.use(session({
 app.use(express.static(path.join(__dirname, 'public')));
 if(coins[config.coin].api) {
 	var rateLimit = require("express-rate-limit");
+	var limiter = rateLimit(apiProperties.limit);
 	var apiProperties = coins[config.coin].api();
 	var apiRounter = express.Router();
 	app.use(apiProperties.base_uri, limiter);
