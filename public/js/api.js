e6d5e3b91c8558bc6a5f73c514e4c623d43d4453
Tri Nguyen <tri282@gmail.com>
2019-11-24 18:14:30 -0800

    added url info for api calls

--- public/js/api.js
+++ public/js/api.js
@@ -1,12 +1,15 @@
-function displayAPIInfo(id, api) {
+function displayAPIInfo(id, api, baseUrl) {
 	if(!(api instanceof Object)) {
 		api = JSON.parse(api);
 	}
-	var infoHtml = `<li>${api.description}</li>`;
+	var infoHtml = `
+	<li>${window.location.origin}${baseUrl}${api.uri}<li>
+	<li>Description: ${api.description}</li>
+	`;
 	if(api.params) {
 		infoHtml += "<li> Parameters:<ul>";
 		for(var i in api.params) {
-			infoHtml += `<li>${api.params[i].name} - ${api.params[i].type}: {api.params[i].description}</li>`;
+			infoHtml += `<li>${api.params[i].name} - ${api.params[i].type}: ${api.params[i].description}</li>`;
 		}
 		infoHtml += "</ul></li>";
 	}
