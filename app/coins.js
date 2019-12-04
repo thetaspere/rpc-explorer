ba7f280cb30ba61a625bb6a4f76a8463a0c569ac
Raptor3um <47279850+Raptor3um@users.noreply.github.com>
2019-12-04 12:34:00 -0500

    add rtm

--- app/coins.js
+++ app/coins.js
@@ -1,11 +1,13 @@
 var btc = require("./coins/btc.js");
 var ltc = require("./coins/ltc.js");
 var pgn = require("./coins/pgn.js");
+var pgn = require("./coins/rtm.js");
 
 module.exports = {
 	"BTC": btc,
 	"LTC": ltc,
 	"PGN": pgn,
+	"RTM": rtm,
 
-	"coins":["BTC", "LTC", "PGN"]
+	"coins":["BTC", "LTC", "PGN", "RTM"]
 };
