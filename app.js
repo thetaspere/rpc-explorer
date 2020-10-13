55257d7f5949d13114281579a6b5c009636e267f
Tri Nguyen <tri282@gmail.com>
2020-10-13 16:27:44 -0700

    comment out btcEventListeners for now

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var utils = require("./app/utils.js");
 var moment = require("moment");
 var Decimal = require('decimal.js');
 var bitcoinCore = require("bitcoin-core");
-var BTCEventListener = require("./app/api/btcEventListeners.js");
+//var BTCEventListener = require("./app/api/btcEventListeners.js");
 var pug = require("pug");
 var momentDurationFormat = require("moment-duration-format");
 var coreApi = require("./app/api/coreApi.js");
@@ -217,13 +217,13 @@ app.continueStartup = function() {
 	};
 
 	global.rpcClientNoTimeout = new bitcoinCore(rpcClientNoTimeoutProperties);
-	var btcEventListener = new BTCEventListener({
+	/*var btcEventListener = new BTCEventListener({
 		ip : rpcCred.host,
 		zmq_port : 23024,
 		network : coins.networks[global.coinConfig.ticker],
 		masternodeSupported : global.coinConfig.masternodeSupported
 	});
-	btcEventListener.listen();
+	btcEventListener.listen();*/
 
 	if(global.coinConfig.masternodeSupported) {
 		utils.scheduleCheckIps();
